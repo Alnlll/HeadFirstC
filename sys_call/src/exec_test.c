f838e52f412158f59c36bff8914904a00960255c
Unknown <alnlll@126.com>
2017-09-05 09:02:04 +0800

    Add Code: test sys call with fork().

--- sys_call/src/exec_test.c
+++ sys_call/src/exec_test.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+typedef struct
+{
+	char *diner;
+	char *env[2];
+} cmd;
 
 int main(void)
 {
+	int i = 0;
+	pid_t pid = 0;
+
+	cmd test[3] = 
+	{
+		{"apple", {"JUICE=peach", NULL}},
+		{"banana", {"JUICE=apple", NULL}},
+		{"pinapple", {"JUICE=strawberry", NULL}},
+	};
+
 	char *my_env[] = {"JUICE=peach&apple", NULL};
 
-	execle("./diner", "./diner", "4", NULL, my_env);
+	for (i = 0; i < sizeof(test) / sizeof(test[0]); i++)
+	{
+		pid = fork();
+		if (-1 == pid)
+		{
+			fprintf(stderr, "Fork fail: %s\n", strerror(errno));
+			return -1;
+		}
+
+		if (!pid)
+		{
+			printf("index:%d\n", i);
+			if (-1 == execle("./diner", "./diner", test[i].diner, NULL, test[i].env))
+			{
+				fprintf(stderr, "Execl fail: %s\n", strerror(errno));
+				return -1;
+			}
+			return 0;
+		}
+	}
 
 	return 0;
+
 }
